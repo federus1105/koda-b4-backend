017f6928f9edb342e742ea7325b4fdb1644edacf
federus1105 <federusrudi@gmail.com>
2025-11-07 15:11:35 +0700

    Feat ( routes ) : init function routes

---
+++ internals/routes/router.go
@@ -0,0 +1,31 @@
+package routes
+
+import (
+	"github.com/federus1105/koda-b4-backend/internals/models"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	docs "github.com/federus1105/koda-b4-backend/docs"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+func InitRouter(db *pgxpool.Pool) *gin.Engine {
+	router := gin.Default()
+	router.Use(gin.Recovery())
+
+	// --- SWAGGER ---
+	docs.SwaggerInfo.BasePath = "/"
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
+	router.Static("/img", "public")
+
+	router.NoRoute(func(ctx *gin.Context) {
+		ctx.JSON(404, models.Response{
+			Status:  false,
+			Message: "Route Not Found, Try Again!",
+		})
+	})
+	return router
+
+}
