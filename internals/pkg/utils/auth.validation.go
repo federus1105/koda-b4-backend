f9da4908a74add1003ee874b26728a18ba803078
federus1105 <federusrudi@gmail.com>
2025-11-08 17:41:40 +0700

    Feat ( utils ) : validation for login field

--- internals/pkg/utils/auth.validation.go
+++ internals/pkg/utils/auth.validation.go
@@ -19,3 +19,17 @@ func ErrorRegisterMsg(fe validator.FieldError) string {
 		return field + " is invalid"
 	}
 }
+
+func ErrorLoginMsg(fe validator.FieldError) string {
+	field := fe.Field()
+	switch fe.Tag() {
+	case "required":
+		return field + " is required"
+	case "password_complex":
+		return field + " must contain uppercase, lowercase, number, and special character"
+	case "email":
+		return "invalid email format"
+	default:
+		return field + " is invalid"
+	}
+}
