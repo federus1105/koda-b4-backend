b290dbb922522d51c7344ebe62232acb317d0ba4
federus1105 <federusrudi@gmail.com>
2025-11-08 23:49:29 +0700

    Feat ( models ) : created product, which handles logic with the database

--- internals/models/product.model.go
+++ internals/models/product.model.go
@@ -3,6 +3,8 @@ package models
 import (
 	"context"
 	"fmt"
+	"log"
+	"mime/multipart"
 	"strings"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -15,6 +17,35 @@ type Product struct {
 	Price       string `json:"price"`
 	Description string `json:"description"`
 	Stock       string `json:"stock"`
+}
+
+type CreateProducts struct {
+	Id             int                   `form:"id"`
+	Name           string                `form:"name" binding:"required"`
+	ImageId        int                   `form:"imageId"`
+	Image_one      *multipart.FileHeader `form:"image_one" binding:"required"`
+	Image_two      *multipart.FileHeader `form:"image_two"`
+	Image_three    *multipart.FileHeader `form:"image_three"`
+	Image_four     *multipart.FileHeader `form:"image_four"`
+	Image_oneStr   string                `form:"image_oneStr"`
+	Image_twoStr   string                `form:"image_twoStr,omitempty"`
+	Image_threeStr string                `form:"image_threeStr,omitempty"`
+	Image_fourStr  string                `form:"image_fourStr,omitempty"`
+	Price          float64               `form:"price" binding:"required,gte=5000"`
+	Rating         float64               `form:"rating" binding:"required,gte=1,lte=10"`
+	Description    string                `form:"description" binding:"required"`
+	Stock          int                   `form:"stock" binding:"gte=0"`
+}
+
+type ProductResponse struct {
+	ID          int               `json:"id"`
+	Name        string            `json:"name"`
+	ImageID     int               `json:"idImage"`
+	Images      map[string]string `json:"images,omitempty"`
+	Price       float64           `json:"price"`
+	Rating      float64           `json:"rating"`
+	Description string            `json:"description"`
+	Stock       int               `json:"stock"`
 }
 
 func GetListProduct(ctx context.Context, db *pgxpool.Pool, name string, limit, offset int) ([]Product, error) {
@@ -61,4 +92,64 @@ func GetListProduct(ctx context.Context, db *pgxpool.Pool, name string, limit, o
 	}
 
 	return products, nil
+}
+
+func CreateProduct(ctx context.Context, db *pgxpool.Pool, body CreateProducts) (CreateProducts, error) {
+	// --- START QUERY TRANSACTION ---
+	tx, err := db.Begin(ctx)
+	if err != nil {
+		log.Println("Failed to begin transaction:", err)
+		return CreateProducts{}, err
+	}
+	defer tx.Rollback(ctx)
+
+	// --- INSERT PRODUCT IMAGES ---
+	imageSQL := `
+        INSERT INTO product_images (photos_one, photos_two, photos_three, photos_four)
+        VALUES ($1, $2, $3, $4)
+        RETURNING id
+    `
+	var imageId int
+	if err := tx.QueryRow(ctx, imageSQL,
+		body.Image_oneStr,
+		body.Image_twoStr,
+		body.Image_threeStr,
+		body.Image_fourStr,
+	).Scan(&imageId); err != nil {
+		log.Println("Failed to insert product_images:", err)
+		return CreateProducts{}, err
+	}
+
+	// --- INSERT PRODUCT
+	productSQL := `INSERT INTO product (name, description, rating, priceoriginal, stock, id_product_images) 
+	VALUES ($1, $2, $3, $4, $5, $6)
+	RETURNING id, name, description, rating, priceoriginal, stock, id_product_images`
+	values := []any{body.Name, body.Description, body.Rating, body.Price, body.Stock, imageId}
+	var newProduct CreateProducts
+	if err := tx.QueryRow(ctx, productSQL, values...).Scan(
+		&newProduct.Id,
+		&newProduct.Name,
+		&newProduct.Description,
+		&newProduct.Rating,
+		&newProduct.Price,
+		&newProduct.Stock,
+		&newProduct.ImageId,
+	); err != nil {
+		log.Println("Failed to insert product:", err)
+		return CreateProducts{}, err
+	}
+
+	// --- ASIGN IMAGE STR TO RETURN STRUCT RESPONSE---
+	newProduct.Image_oneStr = body.Image_oneStr
+	newProduct.Image_twoStr = body.Image_twoStr
+	newProduct.Image_threeStr = body.Image_threeStr
+	newProduct.Image_fourStr = body.Image_fourStr
+
+	// --- COMMIT TRANSACTION ---
+	if err := tx.Commit(ctx); err != nil {
+		log.Println("Failed to commit transaction:", err)
+		return CreateProducts{}, err
+	}
+
+	return newProduct, nil
 }
