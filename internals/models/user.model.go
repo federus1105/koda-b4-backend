d5ac7967295ae997c357c0fe2883647c7c04bed3
federus1105 <federusrudi@gmail.com>
2025-11-09 20:06:32 +0700

    Feat ( models ) : create user feature that handles query logic to the database using SQL transactions

--- internals/models/user.model.go
+++ internals/models/user.model.go
@@ -3,6 +3,8 @@ package models
 import (
 	"context"
 	"fmt"
+	"log"
+	"mime/multipart"
 	"strings"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -14,6 +16,18 @@ type UserList struct {
 	Phone    string `json:"phone"`
 	Address  string `json:"address"`
 	Email    string `json:"email"`
+}
+
+type UserBody struct {
+	Id        int                   `form:"id"`
+	Photos    *multipart.FileHeader `form:"photos" binding:"required"`
+	PhotosStr string                `form:"photosStr"`
+	Fullname  string                `form:"fullname" binding:"required,max=30"`
+	Email     string                `form:"email"  binding:"required,email"`
+	Phone     string                `form:"phone" binding:"max=12"`
+	Password  string                `form:"password" binding:"required,password_complex"`
+	Address   string                `form:"address" binding:"required,max=50"`
+	Role      string                `form:"role" binding:"required,oneof=user admin"`
 }
 
 func GetListUser(ctx context.Context, db *pgxpool.Pool, name string, limit, offset int) ([]UserList, error) {
@@ -60,4 +74,57 @@ func GetListUser(ctx context.Context, db *pgxpool.Pool, name string, limit, offs
 	}
 
 	return users, nil
+}
+
+func CreateUser(ctx context.Context, db *pgxpool.Pool, hashPassword string, user UserBody) (UserBody, error) {
+	// --- START TRANSACTION ---
+	tx, err := db.Begin(ctx)
+	if err != nil {
+		log.Println("Failed to begin transaction : ", err)
+		return UserBody{}, err
+	}
+	defer tx.Rollback(ctx)
+	var userID int
+
+	// --- INSERT TABLE USER ---
+	userSQL := `INSERT INTO users (email, password, role) VALUES ($1, $2, $3) 
+		RETURNING id`
+
+	if err := tx.QueryRow(ctx, userSQL,
+		user.Email,
+		hashPassword,
+		user.Role).Scan(&userID); err != nil {
+		log.Println("Failed to insert user :", err)
+		return UserBody{}, err
+	}
+
+	// --- INSERT TABLE ACCOUNT ---
+	accountSQL := `INSERT INTO account (id_users, fullname, phoneNumber, address, photos) 
+	VALUES ($1, $2, $3, $4, $5) RETURNING id_users, fullname, phoneNumber, address, photos`
+
+	values := []any{userID, user.Fullname, user.Phone, user.Address, user.PhotosStr}
+	var newUser UserBody
+	if err := tx.QueryRow(ctx, accountSQL, values...).Scan(
+		&newUser.Id,
+		&newUser.Fullname,
+		&newUser.Phone,
+		&newUser.Address,
+		&newUser.PhotosStr,
+	); err != nil {
+		log.Println("Failed to insert product:", err)
+		return UserBody{}, err
+	}
+
+	// --- ASIGN IMAGE STR TO RETURN STRUCT RESPONSE--
+	newUser.PhotosStr = user.PhotosStr
+	newUser.Email = user.Email
+	newUser.Role = user.Role
+
+	// --- COMMIT TRANSACTION ---
+	if err = tx.Commit(ctx); err != nil {
+		log.Println("Failed to commit Transaction : ", err)
+		return UserBody{}, err
+	}
+
+	return newUser, nil
 }
