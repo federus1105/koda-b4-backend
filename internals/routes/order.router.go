7abf8ba9c8a41b4e318b3ad203d652433b61f29d
federus1105 <federusrudi@gmail.com>
2025-11-09 14:20:50 +0700

    Feat ( routes ) : add router init order function with router post /list

---
+++ internals/routes/order.router.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/federus1105/koda-b4-backend/internals/controllers"
+	"github.com/federus1105/koda-b4-backend/internals/middlewares"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func InitOrderRouter(router *gin.Engine, db *pgxpool.Pool) {
+	orderRouter := router.Group("/admin/order")
+
+	orderRouter.POST("/list", middlewares.VerifyToken, func(ctx *gin.Context) {
+		controllers.GetListOrder(ctx, db)
+	})
+}
