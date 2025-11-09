fb310ec8e3bd5c94ae61fd1424d12c7dc2798e10
federus1105 <federusrudi@gmail.com>
2025-11-10 01:16:57 +0700

    Feat ( utils ) : added auth struct feature for swagger requests to make them more structured

---
+++ internals/pkg/utils/swagger.response.go
@@ -0,0 +1,14 @@
+package utils
+
+// === AUTH ===
+type LoginRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
+
+type RegisterRequest struct {
+	Fullname string `json:"fullname"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
