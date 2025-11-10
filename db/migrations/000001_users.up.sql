fb0332ad649b710f424454959e79fddeace244f0
federus1105 <federusrudi@gmail.com>
2025-11-10 10:09:56 +0700

    fix ( migrations ) : delete migration now because want to make changes

--- db/migrations/000001_users.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TYPE role AS ENUM ('admin', 'user');
-
-CREATE TABLE users (
-	id serial4 NOT NULL,
-	email varchar(100) NOT NULL,
-	password varchar(100) NOT NULL,
-	role role NULL,
-	CONSTRAINT users_pkey PRIMARY KEY (id)
-);
