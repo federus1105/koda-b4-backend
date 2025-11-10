b14e88740681817d577a97e41ce17303740385d5
federus1105 <federusrudi@gmail.com>
2025-11-10 17:39:52 +0700

    Feat ( routes ) : feature init router categories

---
+++ internals/routes/categories.router.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/federus1105/koda-b4-backend/internals/controllers"
+	"github.com/federus1105/koda-b4-backend/internals/middlewares"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func InitCategoriesRouter(router *gin.Engine, db *pgxpool.Pool) {
+	categoriesRouter := router.Group("/admin/categories")
+
+	categoriesRouter.GET("", middlewares.VerifyToken, func(ctx *gin.Context) {
+		controllers.GetListCategories(ctx, db)
+	})
+}
