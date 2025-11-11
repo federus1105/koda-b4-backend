c95dfc778c45e621dc0c35044bfe642f518b4068
federus1105 <federusrudi@gmail.com>
2025-11-11 15:00:58 +0700

    Feat ( controllers ) : feature create images for product

--- internals/controllers/images.controllers.go
+++ internals/controllers/images.controllers.go
@@ -3,11 +3,15 @@ package controllers
 import (
 	"context"
 	"fmt"
+	"log"
+	"mime/multipart"
 	"net/http"
 	"strconv"
 	"time"
 
 	"github.com/federus1105/koda-b4-backend/internals/models"
+	"github.com/federus1105/koda-b4-backend/internals/pkg/libs"
+	"github.com/federus1105/koda-b4-backend/internals/pkg/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -19,7 +23,7 @@ func GetListImageById(ctx *gin.Context, db *pgxpool.Pool) {
 	if err != nil {
 		ctx.JSON(404, models.Response{
 			Success: false,
-			Message: "Invalid product id",
+			Message: "Invalid images id",
 		})
 		return
 	}
@@ -50,6 +54,115 @@ func GetListImageById(ctx *gin.Context, db *pgxpool.Pool) {
 	ctx.JSON(200, models.ResponseSucces{
 		Success: true,
 		Message: "Get data succesfully",
+		Result:  images,
+	})
+}
+
+func CreateImagesbyId(ctx *gin.Context, db *pgxpool.Pool) {
+	var body models.ImagesBody
+	// --- GET PORDUCT ID ---
+	productIDstr := ctx.Param("id")
+	productID, err := strconv.Atoi(productIDstr)
+	if err != nil {
+		ctx.JSON(404, models.Response{
+			Success: false,
+			Message: "Invalid product id",
+		})
+		return
+	}
+
+	// --- CHECKING CLAIMS TOKEN ---
+	claims, exists := ctx.Get("claims")
+	if !exists {
+		fmt.Println("ERROR :", !exists)
+		ctx.AbortWithStatusJSON(403, models.Response{
+			Success: false,
+			Message: "Please log in again",
+		})
+		return
+	}
+	user, ok := claims.(libs.Claims)
+	if !ok {
+		fmt.Println("ERROR", !ok)
+		ctx.AbortWithStatusJSON(500, models.Response{
+			Success: false,
+			Message: "An error occurred!, please try again.",
+		})
+		return
+	}
+
+	// --- UPLOAD IMAGES ---
+	imageFiles := map[string]*multipart.FileHeader{
+		"photos_one":   body.ImagesOne,
+		"photos_two":   body.ImagesTwo,
+		"photos_three": body.ImagesThree,
+		"photos_four":  body.ImagesFour,
+	}
+
+	imageStrs := map[string]string{}
+
+	// --- MULTIPLE UPLOAD IMAGES ---
+	for key, file := range imageFiles {
+		if file == nil {
+			continue
+		}
+		savePath, generatedFilename, err := utils.UploadImageFile(ctx, file, "public", fmt.Sprintf("%s_%d", key, user.ID))
+		if err != nil {
+			ctx.JSON(400, models.Response{
+				Success: false,
+				Message: err.Error(),
+			})
+			return
+		}
+
+		if err := ctx.SaveUploadedFile(file, savePath); err != nil {
+			ctx.JSON(500, models.Response{
+				Success: false,
+				Message: fmt.Sprintf("Failed to save %s", key),
+			})
+			return
+		}
+
+		imageStrs[key] = generatedFilename
+	}
+
+	// --- ASSIGN TO BODY ---
+	body.ImagesOneStr = imageStrs["photos_one"]
+	body.ImagesTwoStr = imageStrs["photos_two"]
+	body.ImagesThreeStr = imageStrs["photos_three"]
+	body.ImagesFourStr = imageStrs["photos_four"]
+
+	// ---- LIMITS QUERY EXECUTION TIME ---
+	ctxTimeout, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	images, err := models.CreateImagesbyId(ctxTimeout, db, body, productID)
+	if err != nil {
+		log.Println("ERROR : ", err)
+		ctx.JSON(500, models.Response{
+			Success: false,
+			Message: "An error occurred while saving data",
+		})
+		return
+	}
+
+	//  ---- ASIGN LIST IMAGE ---
+	image := make(map[string]string)
+	if images.ImagesOneStr != "" {
+		image["image_one"] = images.ImagesOneStr
+	}
+	if images.ImagesTwoStr != "" {
+		image["image_two"] = images.ImagesTwoStr
+	}
+	if images.ImagesThreeStr != "" {
+		image["image_three"] = images.ImagesThreeStr
+	}
+	if images.ImagesFourStr != "" {
+		image["image_four"] = images.ImagesFourStr
+	}
+
+	ctx.JSON(200, models.ResponseSucces{
+		Success: true,
+		Message: "Created Images Succesfully",
 		Result:  images,
 	})
 }
--- internals/models/images.model.go
+++ internals/models/images.model.go
@@ -3,6 +3,8 @@ package models
 import (
 	"context"
 	"fmt"
+	"log"
+	"mime/multipart"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -12,6 +14,17 @@ type Images struct {
 	ImagesTwo   *string `form:"imagesTwo"`
 	ImagesThree *string `form:"imagesThree"`
 	ImagesFour  *string `form:"imagesFour"`
+}
+
+type ImagesBody struct {
+	ImagesOne      *multipart.FileHeader `form:"imagesOne"`
+	ImagesTwo      *multipart.FileHeader `form:"imagesTwo"`
+	ImagesThree    *multipart.FileHeader `form:"imagesThree"`
+	ImagesFour     *multipart.FileHeader `form:"imagesFour"`
+	ImagesOneStr   string                `form:"imagesOneStr"`
+	ImagesTwoStr   string                `form:"imagesTwoStr"`
+	ImagesThreeStr string                `form:"imagesThreeStr"`
+	ImagesFourStr  string                `form:"imagesFourStr"`
 }
 
 func GetListImageById(ctx context.Context, db *pgxpool.Pool, idProduct int) ([]Images, error) {
@@ -26,7 +39,6 @@ func GetListImageById(ctx context.Context, db *pgxpool.Pool, idProduct int) ([]I
 		fmt.Println(err)
 	}
 	defer rows.Close()
-
 	var images []Images
 
 	for rows.Next() {
@@ -42,4 +54,28 @@ func GetListImageById(ctx context.Context, db *pgxpool.Pool, idProduct int) ([]I
 	}
 
 	return images, nil
+}
+
+func CreateImagesbyId(ctx context.Context, db *pgxpool.Pool, body ImagesBody, idProduct int) (ImagesBody, error) {
+	sql := `UPDATE product_images (photos_one, photos_two, photos_three, photos_four)
+        VALUES ($1, $2, $3, $4)
+        RETURNING photos_one, photos_two, photos_three, photos_four`
+
+	values := []any{
+		body.ImagesOneStr,
+		body.ImagesTwoStr,
+		body.ImagesThreeStr,
+		body.ImagesFourStr,
+	}
+	var newImages ImagesBody
+	if err := db.QueryRow(ctx, sql, values...).Scan(
+		&newImages.ImagesOneStr,
+		&newImages.ImagesTwoStr,
+		&newImages.ImagesThreeStr,
+		&newImages.ImagesFourStr); err != nil {
+		log.Println("Failed to insert product_images:", err)
+		return ImagesBody{}, err
+	}
+	return newImages, nil
+
 }
