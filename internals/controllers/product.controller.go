aea407b5a30f66e06b335a6fb139dbe99d66c811
federus1105 <federusrudi@gmail.com>
2025-11-11 15:26:07 +0700

    Feat ( conttrollers ) : get favorite product feature

--- internals/controllers/product.controller.go
+++ internals/controllers/product.controller.go
@@ -441,3 +441,51 @@ func DeleteProduct(ctx *gin.Context, db *pgxpool.Pool) {
 		Result:  fmt.Sprintf("product id %d", productId),
 	})
 }
+
+// GetListProductFavorite godoc
+// @Summary Get list products Favorite
+// @Description Get paginated list of products with pagination
+// @Tags Products
+// @Param page query int false "Page number" default(1)
+// @Success 200 {object} models.ResponseSucces
+// @Router /favorite-product [get]
+func GetListFavoriteProduct(ctx *gin.Context, db *pgxpool.Pool) {
+	// --- GET QUERY PARAMS ---
+	pageStr := ctx.Query("page")
+	page, err := strconv.Atoi(pageStr)
+	if err != nil || page < 1 {
+		page = 1
+	}
+
+	limit := 3
+	offset := (page - 1) * limit
+
+	// ---- LIMITS QUERY EXECUTION TIME ---
+	ctxTimeout, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	products, err := models.GetListFavoriteProduct(ctxTimeout, db, limit, offset)
+	if err != nil {
+		ctx.JSON(500, models.Response{
+			Success: false,
+			Message: "Failed Get list data products",
+		})
+		fmt.Println("Error : ", err.Error())
+		return
+	}
+
+	// --- VALIDATION FOR LIST PRODUCT ---
+	if len(products) == 0 {
+		ctx.JSON(200, gin.H{
+			"success": true,
+			"data":    []string{},
+			"message": "Not found list product favorite",
+		})
+		return
+	}
+
+	ctx.JSON(200, models.ResponseSucces{
+		Success: true,
+		Message: "Get data succesfully",
+		Result:  products,
+	})
+}
