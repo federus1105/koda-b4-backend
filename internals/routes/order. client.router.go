52f45d94a0824adcf3092fe5dc073d2452fb4ab9
federus1105 <federusrudi@gmail.com>
2025-11-12 10:50:04 +0700

    Feat ( router ) : init router order client router

---
+++ internals/routes/order. client.router.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/federus1105/koda-b4-backend/internals/controllers"
+	"github.com/federus1105/koda-b4-backend/internals/middlewares"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func InitOrderClientRoutes(router *gin.Engine, db *pgxpool.Pool) {
+	InitOrderClientRoutes := router.Group("")
+
+	InitOrderClientRoutes.POST("/cart", middlewares.VerifyToken, middlewares.AuthMiddleware(), func(ctx *gin.Context) {
+		controllers.CreateCartProduct(ctx, db)
+	})
+}
