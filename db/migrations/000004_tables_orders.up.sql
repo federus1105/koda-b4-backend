2a91f6fa305d7a535c9bad0bc54bc71a8ab2f549
federus1105 <federusrudi@gmail.com>
2025-11-13 09:47:36 +0700

    Feat ( migrations ) : migrations table status for orders reference

--- db/migrations/000004_tables_orders.up.sql
+++ db/migrations/000004_tables_orders.up.sql
@@ -3,6 +3,12 @@ CREATE TABLE delivery (
     name varchar(100) NOT NULL
 );
 
+
+CREATE TABLE status (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(20) NOT NULL DEFAULT 'on progres'
+);
+
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
     id_account INT NOT NULL,
@@ -13,7 +19,7 @@ CREATE TABLE orders (
     email VARCHAR(100) NOT NULL,
     id_delivery INT NOT NULL,
     total FLOAT NOT NULL,
-    status BOOLEAN NOT NULL,
+    id_status INT NOT NULL,
     order_number VARCHAR(50) NOT NULL,
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -23,3 +29,5 @@ ALTER TABLE orders ADD FOREIGN KEY(id_delivery) REFERENCES delivery(id);
 ALTER TABLE orders ADD FOREIGN KEY (id_account) REFERENCES account(id);
 
 ALTER TABLE orders ADD FOREIGN KEY (id_paymentmethod) REFERENCES payment_method(id);
+
+ALTER TABLE orders ADD FOREIGN KEY(id_status) REFERENCES status(id);
