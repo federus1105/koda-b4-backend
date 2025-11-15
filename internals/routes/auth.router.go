3dc70ef9300fb75f76e6583ae1ea72feedbb16c5
federus1105 <federusrudi@gmail.com>
2025-11-16 03:14:59 +0700

    Feat ( routes ) : init feature forgot password

--- internals/routes/auth.router.go
+++ internals/routes/auth.router.go
@@ -4,9 +4,10 @@ import (
 	"github.com/federus1105/koda-b4-backend/internals/controllers"
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
 )
 
-func InitAuthRouter(router *gin.Engine, db *pgxpool.Pool) {
+func InitAuthRouter(router *gin.Engine, db *pgxpool.Pool, rdb *redis.Client) {
 	authRouter := router.Group("/auth")
 
 	authRouter.POST("/register", func(ctx *gin.Context) {
@@ -14,6 +15,13 @@ func InitAuthRouter(router *gin.Engine, db *pgxpool.Pool) {
 	})
 	authRouter.POST("/login", func(ctx *gin.Context) {
 		controllers.Login(ctx, db)
+	})
+	authRouter.POST("/forgot-password", func(ctx *gin.Context) {
+		controllers.ForgotPassword(ctx, db, rdb)
+	})
+
+	authRouter.POST("/reset-password", func(ctx *gin.Context) {
+		controllers.ResetPassword(ctx, db, rdb)
 	})
 
 }
