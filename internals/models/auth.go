7f58e5bd6378cf86b5994b1f345b169e40727980
federus1105 <federusrudi@gmail.com>
2025-11-15 13:06:39 +0700

    fix : move the profile update feature to the profile

--- internals/models/auth.go
+++ internals/models/auth.go
@@ -3,10 +3,8 @@ package models
 import (
 	"context"
 	"errors"
-	"fmt"
 	"log"
 	"mime/multipart"
-	"strings"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -88,86 +86,4 @@ func Login(ctx context.Context, db *pgxpool.Pool, email string) (AuthLogin, erro
 		return AuthLogin{}, err
 	}
 	return user, nil
-}
-
-func UpdateProfile(ctx context.Context, db *pgxpool.Pool, input ProfileUpdate, Id int) (ProfileUpdate, error) {
-	// --- START QUERY TRANSACTION ---
-	tx, err := db.Begin(ctx)
-	if err != nil {
-		return ProfileUpdate{}, err
-	}
-	defer tx.Rollback(ctx)
-
-	// --- DYNAMIC SET USER ---
-	setClauses := []string{}
-	args := []any{}
-	idx := 1
-
-	if input.Fullname != nil {
-		setClauses = append(setClauses, fmt.Sprintf("fullname=$%d", idx))
-		args = append(args, *input.Fullname)
-		idx++
-	}
-
-	if input.Phone != nil {
-		setClauses = append(setClauses, fmt.Sprintf("phoneNumber=$%d", idx))
-		args = append(args, *input.Phone)
-		idx++
-	}
-
-	if input.Address != nil {
-		setClauses = append(setClauses, fmt.Sprintf("address=$%d", idx))
-		args = append(args, *input.Address)
-		idx++
-	}
-
-	if input.PhotosStr != nil {
-		setClauses = append(setClauses, fmt.Sprintf("photos=$%d", idx))
-		args = append(args, *input.PhotosStr)
-		idx++
-	}
-
-	if len(setClauses) > 0 {
-		query := fmt.Sprintf("UPDATE account SET %s WHERE id=$%d", strings.Join(setClauses, ","), idx)
-		args = append(args, Id)
-		_, err := tx.Exec(ctx, query, args...)
-		if err != nil {
-			log.Println("Failed to update account:", err)
-			return ProfileUpdate{}, err
-		}
-	}
-
-	// --- GET UPDATED DATA ---
-	var updated ProfileUpdate
-	querySelect := `
-		SELECT 
-		u.id,
-        u.email,
-		COALESCE(a.fullname, '-'),
-		COALESCE(a.phoneNumber, '-'),
-		COALESCE(a.address, '-'),
-		COALESCE(a.photos, '-')
-	FROM account a
-	JOIN users u ON u.id = a.id_users
-	WHERE a.id = $1
-	`
-	if err := tx.QueryRow(ctx, querySelect, Id).Scan(
-		&updated.Id,
-		&updated.Email,
-		&updated.Fullname,
-		&updated.Phone,
-		&updated.Address,
-		&updated.PhotosStr,
-	); err != nil {
-		log.Println("Failed to fetch updated user:", err)
-		return ProfileUpdate{}, err
-	}
-
-	// --- COMMIT TRANSACTION ---
-	if err := tx.Commit(ctx); err != nil {
-		log.Println("Failed to commit transaction:", err)
-		return ProfileUpdate{}, err
-	}
-
-	return updated, nil
 }
