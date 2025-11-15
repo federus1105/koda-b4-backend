2b5646a618b67b44007b332a66774db0099288e4
federus1105 <federusrudi@gmail.com>
2025-11-16 03:23:59 +0700

    Feat ( models ) : small features that support forgot password

--- internals/models/auth.go
+++ internals/models/auth.go
@@ -4,9 +4,16 @@ import (
 	"context"
 	"errors"
 	"log"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
+)
+
+var (
+	DB  *pgxpool.Pool
+	RDB *redis.Client
 )
 
 type AuthRegister struct {
@@ -21,6 +28,21 @@ type AuthLogin struct {
 	Email    string `json:"email"  binding:"required,email"`
 	Password string `json:"password" binding:"required,password_complex"`
 	Role     string `json:"role,omitempty"`
+}
+
+type Users struct {
+	Id       int    `json:"id"`
+	Email    string `json:"email" binding:"email"`
+	Password string `json:"password" binding:"password_complex"`
+}
+
+type ReqResetPassword struct {
+	Token       string `json:"token" binding:"required"`
+	NewPassword string `json:"new_password" binding:"password_complex"`
+}
+
+type ReqForgot struct {
+	Email string `json:"email" binding:"required,email"`
 }
 
 func Register(ctx context.Context, db *pgxpool.Pool, hashed string, user AuthRegister) (AuthRegister, error) {
@@ -75,4 +97,37 @@ func Login(ctx context.Context, db *pgxpool.Pool, email string) (AuthLogin, erro
 		return AuthLogin{}, err
 	}
 	return user, nil
+}
+
+// ----- GET USER BY EMAIL -----
+func GetUserByEmail(ctx context.Context, db *pgxpool.Pool, email string) (*Users, error) {
+	query := "SELECT id, email, password FROM users WHERE email=$1"
+	var user Users
+	if err := db.QueryRow(ctx, query, email).Scan(&user.Id, &user.Email, &user.Password); err != nil {
+		return nil, errors.New("user not found")
+	}
+	return &user, nil
+}
+
+// --- SAVE TOKEN FOR RESET PASSWORD ---
+func SaveResetToken(ctx context.Context, rdb *redis.Client, key, userID string, ttl time.Duration) error {
+	return rdb.Set(ctx, key, userID, ttl).Err()
+}
+
+// --- GET USER ID FROM TOKEN REDIS ---
+func GetUserIDFromToken(ctx context.Context, rdb *redis.Client, key string) (string, error) {
+	val, err := rdb.Get(ctx, key).Result()
+	if err != nil {
+		return "", errors.New("invalid or expired token")
+	}
+	return val, nil
+}
+
+// --- UPDATE PASSWORD ---
+func UpdatePasswordByID(ctx context.Context, db *pgxpool.Pool, userID int, hashedPassword string) error {
+	_, err := db.Exec(ctx,
+		"UPDATE users SET password = $1 WHERE id = $2",
+		hashedPassword, userID,
+	)
+	return err
 }
