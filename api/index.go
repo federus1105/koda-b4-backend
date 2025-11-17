6e1be6dc43e63126b301945a19158c7fc6cef324
federus1105 <federusrudi@gmail.com>
2025-11-17 17:39:32 +0700

    Feat : init cloudinary

--- api/index.go
+++ api/index.go
@@ -3,7 +3,9 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"os"
 
+	"github.com/cloudinary/cloudinary-go/v2"
 	"github.com/federus1105/koda-b4-backend/internals/configs"
 	"github.com/federus1105/koda-b4-backend/internals/routes"
 	"github.com/gin-gonic/gin"
@@ -23,17 +25,29 @@ func setupApp() *gin.Engine {
 	app := gin.New()
 	app.Use(gin.Recovery())
 
+	// --- CONNECT DATABASE ---
 	db, err := configs.ConnectDB()
 	if err != nil {
 		panic("DB connection failed: " + err.Error())
 	}
 
+	// --- CONNECT REDIS ---
 	rdb, err := configs.NewRedis()
 	if err != nil {
 		panic("Redis connection failed: " + err.Error())
 	}
 
-	routes.InitRouter(app, db, rdb)
+	// --- INIT CLOUDINARY ---
+	var cld *cloudinary.Cloudinary
+	if os.Getenv("CLOUDINARY_URL") != "" {
+		cld, err = cloudinary.NewFromURL(os.Getenv("CLOUDINARY_URL"))
+		if err != nil {
+			panic("Cloudinary connection failed: " + err.Error())
+		}
+		fmt.Println("✅ Cloudinary connected")
+	}
+
+	routes.InitRouter(app, db, rdb, cld)
 	app.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"Success": true,
